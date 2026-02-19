65a395b577144502e884191eade92be31926c6cb
im-devendhar <devendharyadav781@gmail.com>
2026-02-19 15:42:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => res.status(200).json({ status: 'ok' }));
-app.get('/', (req, res) => res.send('Deployed on ECS, done by Devendhar'));
+app.get('/', (req, res) => res.send('Deployed nodejs application on Fargate, done by Devendhar'));
 
 app.listen(port, '0.0.0.0', () => console.log(`Server listening on ${port}`));
